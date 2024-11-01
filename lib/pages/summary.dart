30b00cba471f3a6e67265758e7754b9d3f4e5960
rafsanislam09 <rafsan.islam010@gmail.com>
2024-11-01 18:58:20 +0600

    Added more documentations in README.md file and added a new feature of BTRFS named Summary

--- lib/pages/summary.dart
+++ lib/pages/summary.dart
@@ -1,13 +1,15 @@
 import 'dart:io';
 //Current Project Files
-import 'package:btrfs_toolkit/universalconsts.dart';
+import 'package:btrfs_toolkit/universalconsts.dart' as commonCode;
 
 void main() async {
   //Introduce the script
   print("This program shows a summary of the root filesystem");
   await Process.run('sh', ['-c', 'btrfs fi df -H -h /']);
+
+  print(commonCode.restartWarning);
   print("Want to restart application?");
   stdout.write("y/N:");
   String? restart = stdin.readLineSync();
-  restartApplication();
+  commonCode.restartApplication(restart);
 }
--- lib/universalconsts.dart
+++ lib/universalconsts.dart
@@ -3,17 +3,22 @@ import 'package:ansicolor/ansicolor.dart';
 //Current Project Files
 import 'package:btrfs_toolkit/pages/homepage.dart' as homePage;
 
-final abc = "Hello";
-final red = AnsiPen()..red();
-final green = AnsiPen()..green();
-final cyan = AnsiPen()..cyan();
-final blue = AnsiPen()..blue();
+//Global Variables
 
-late String restart;
-void restartApplication() {
-  if (restart == "y" || restart == "Y") {
+//Here AnsiPen is a data type provided by "ansicolor" library for coloring text with ANSI escape sequences.
+final hello = "Hello";
+final AnsiPen red = AnsiPen()..red();
+final AnsiPen green = AnsiPen()..green();
+final AnsiPen cyan = AnsiPen()..cyan();
+final AnsiPen blue = AnsiPen()..blue();
+
+//Warning for restarting or closing application
+final String restartWarning = "Selecting N (No) will exit the program.";
+//Function to restart application
+void restartApplication(restartPrompt) {
+  if (restartPrompt == "y" || restartPrompt == "Y") {
     homePage.homePage();
-  } else if (restart == "n" || restart == "N") {
+  } else if (restartPrompt == "n" || restartPrompt == "N") {
     print("Bye!");
     exit(0);
   } else {
